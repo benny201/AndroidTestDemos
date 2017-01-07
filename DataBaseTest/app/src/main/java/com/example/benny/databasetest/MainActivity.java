60e2be66fbdff30b21fabbc44cadfe7bd9c18f02
447093943@qq.com <zwu58@ucsc.edu>
2017-01-06 16:27:16 -0800

    DataBaseTest

--- DataBaseTest/app/src/main/java/com/example/benny/databasetest/MainActivity.java
+++ DataBaseTest/app/src/main/java/com/example/benny/databasetest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.benny.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private Button CrateDataButton;
     private Button UpdateButton;
+    private Button DeleteButton;
+    private Button RetrieveButton;
+    private String TAG = "BENNY!!!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDataBaseHelper = new MyDataBaseHelper(this, "Book.db", null, 4);
+
+        // create database
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // add data
         CrateDataButton = (Button) findViewById(R.id.CrateButton);
         CrateDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,25 +53,58 @@ public class MainActivity extends AppCompatActivity {
                 contentValues.clear();
 
                 // second table
-                //contentValues.put("name", "hello word");
-                //contentValues.put("code", 1);
-                //db.insert("Category", null, contentValues);
-                //contentValues.clear();
+                contentValues.put("author", "B");
+                contentValues.put("pages", 66);
+                contentValues.put("name", "Fuck");
+                db.insert("Book", null, contentValues);
+                contentValues.clear();
 
             }
         });
 
+        // update data
         UpdateButton = (Button) findViewById(R.id.UpdateButton);
         UpdateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SQLiteDatabase db = myDataBaseHelper.getWritableDatabase();
                 ContentValues contentValues = new ContentValues();
-                contentValues.put("pages", 777);
-                db.update("Book", contentValues, "name = ?", new String[] {"Benny's success"});
+                contentValues.put("pages", 77);
+                db.update("Book", contentValues, "name = ?", new String[] {"Fuck"});
 
             }
         });
+
+        //delete data
+        DeleteButton = (Button) findViewById(R.id.deleteButton);
+        DeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = myDataBaseHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[]{"500"});
+            }
+        });
+
+        //Retrieve Data
+        RetrieveButton = (Button) findViewById(R.id.RetrieveData);
+        RetrieveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = myDataBaseHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+
+                        Log.d(TAG, name + author + pages);
+                    } while(cursor.moveToNext());
+                }
+            }
+        });
+
+
     }
 
 
